10e76218133d06926349edd334c14d4f5f57870b
fangbo <fangbo@bilibili.com>
2019-01-25 21:04:45 +0800

    1

--- src/main/java/com/paletter/wlp/dao/PlanDao.java
+++ src/main/java/com/paletter/wlp/dao/PlanDao.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 import com.paletter.easy.sql.EasyConnection;
-import com.paletter.easy.sql.EasySql;
+import com.paletter.easy.sql.EasyDelete;
+import com.paletter.easy.sql.EasyInsert;
+import com.paletter.easy.sql.EasyQuery;
+import com.paletter.easy.sql.EasyUpdate;
 import com.paletter.wlp.entity.Plan;
 
 public class PlanDao {
@@ -19,7 +22,7 @@ public class PlanDao {
 			Connection conn = EasyConnection.getConn();
 			
 			String sql = "select * from plan where date = ?";
-			resultList = EasySql.queryList(conn, sql, Plan.class, query.getDate());
+			resultList = EasyQuery.queryList(conn, sql, Plan.class, query.getDate());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -32,20 +35,20 @@ public class PlanDao {
 		
 		Connection conn = EasyConnection.getConn();
 		
-		EasySql.insert(conn, plan, "plan");
+		EasyInsert.insert(conn, plan, "plan");
 	}
 	
 	public static void update(Plan plan) {
 		
 		Connection conn = EasyConnection.getConn();
 		
-		EasySql.update(conn, plan, "plan", "id=" + plan.getId());
+		EasyUpdate.update(conn, plan, "plan", "id=" + plan.getId());
 	}
 	
 	public static void delete(int id) {
 
 		Connection conn = EasyConnection.getConn();
 		
-		EasySql.delete(conn, "plan", "id=" + id);
+		EasyDelete.delete(conn, "plan", "id=" + id);
 	}
 }
